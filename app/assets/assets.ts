d74d3797e1bcfbe1f1b3c4440bdb31ea3d8b02e7
Yadee9910 <syadeeshani@gmail.com>
2025-10-31 11:15:41 +0530

    feat:developed who we are section

--- app/assets/assets.ts
+++ app/assets/assets.ts
@@ -1,7 +1,10 @@
 import logo from '@/app/assets/logo.png'
 import footerLogo from '@/app/assets/footLogo.png'
+import img from '@/app/assets/img1.png'
 
 export {
     logo,
-footerLogo
+footerLogo,
+
+img
 }
